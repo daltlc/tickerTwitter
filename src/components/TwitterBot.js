f5c8c8cb6f9bbc4fdf3fd6ad674848e5c5b938ea
daltlc <daltonlcarr@yahoo.com>
2020-05-15 19:16:05 -0700

    real time search functionality

--- src/components/TwitterBot.js
+++ src/components/TwitterBot.js
@@ -2,9 +2,14 @@ import axios from 'axios';
 import React from 'react';
 
 class TwitterBot extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+	}
 	state = {
 		loaded: [],
-		searched: []
+		searched: [],
+		searchedTicker: ''
 	};
 	async componentDidMount() {
 		//Gathering data from heroku API I built and adding tweets to loaded array state
@@ -14,8 +19,35 @@ class TwitterBot extends React.Component {
 			loaded: tweets
 		});
 	}
+	handleChange = (e) => {
+		//Watching input and changing searchedTicker string while typing
+		this.setState({ searchedTicker: e.target.value });
+	};
 	render() {
-		return <div>{this.state.loaded.map((tweet, index) => <p key={index}>{tweet.text}</p>)}</div>;
+		//Trimming searched input to all lowercase and filtering displayed post based on search
+		let loaded = this.state.loaded,
+			searchedTicker = this.state.searchedTicker.trim().toLowerCase();
+		if (searchedTicker.length > 0) {
+			loaded = loaded.filter(function(i) {
+				console.log('typing..');
+				return i.text.toLowerCase().match(searchedTicker);
+			});
+		}
+		return (
+			<div>
+				<input
+					type="text"
+					value={this.state.searchedTicker}
+					onChange={this.handleChange}
+					placeholder="Type here..."
+				/>
+				<ul>
+					{loaded.map(function(i) {
+						return <li>{i.text}</li>;
+					})}
+				</ul>
+			</div>
+		);
 	}
 }
 export default TwitterBot;
