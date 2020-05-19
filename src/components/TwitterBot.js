978d3479a5615fb79f69b08107b0968b197f14ce
daltlc <daltonlcarr@yahoo.com>
2020-05-19 13:34:26 -0700

    ended up not going with recoil, everything state wise all currently lives within TwitterBot component. Can create tab and filter list based on tab selection, need to refine functionality and edge cases

--- src/components/TwitterBot.js
+++ src/components/TwitterBot.js
@@ -1,30 +1,36 @@
 import axios from 'axios';
 import React from 'react';
-import FilterTabs from './FilterTabs';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import StarIcon from '@material-ui/icons/Star';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import { RecoilRoot, atom, selector, useRecoilState, useRecoilValue } from 'recoil';
 import { selectedTabState } from './recoil/Atoms';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeadphones } from '@fortawesome/free-solid-svg-icons';
 
 class TwitterBot extends React.Component {
 	constructor(props) {
 		super(props);
 		this.handleChange = this.handleChange.bind(this);
+		this.handleTabState = this.handleTabState.bind(this);
 	}
 
 	state = {
 		loaded: [],
 		searched: [],
 		searchedTicker: '',
-		addedTickers: [ this.tabsFunctional ]
+		actveTab: '',
+		addedTickers: []
 	};
 	tabsFunctional() {
 		const [ tab, setTab ] = useRecoilState(selectedTabState);
-		return tab;
+		this.setState.searchedTicker(tab);
 	}
 	async componentDidMount() {
 		//Gathering data from heroku API I built and adding tweets to loaded array state
@@ -39,6 +45,16 @@ class TwitterBot extends React.Component {
 		this.setState({ searchedTicker: e.target.value });
 	};
 
+	handleTabChange = (event, newValue) => {
+		// setValue(newValue);
+		this.setState({ tabValue: newValue });
+	};
+
+	handleTabState = (e, data) => {
+		this.setState({ searchedTicker: data });
+		console.log(data);
+	};
+
 	addTicker = () => {
 		// Adding ticker to saved list
 		this.setState((state) => {
@@ -48,9 +64,6 @@ class TwitterBot extends React.Component {
 				searchedTicker: ''
 			};
 		});
-		//Using Facebook's new Recoil state management
-		// const [ tab, setselectedTabState ] = useRecoilState(selectedTabState);
-		// setselectedTabState(this.state.searchedTicker);
 	};
 	removeTicker = () => {
 		this.setState((state) => {});
@@ -85,8 +98,27 @@ class TwitterBot extends React.Component {
 						Add to favorites <StarIcon style={{ marginLeft: 10 + 'px' }} />
 					</Button>
 				</div>
-
-				<FilterTabs tabs={this.state.addedTickers} />
+				{/* This will be the Filter Tabs component and that will import the list thats below the Paper component below */}{' '}
+				<Paper square>
+					<Tabs
+						value={this.state.tabValue}
+						indicatorColor="primary"
+						textColor="primary"
+						onChange={this.handleTabChange}
+					>
+						<Tab value="all" label="All" />
+						{//Mapping through tabs that are added in TwitterBot component and passed down as props to this component
+						this.state.addedTickers.map((i) => {
+							return (
+								// <div className="filter-tab">
+								<Tab value={i} label={i} onClick={(e) => this.handleTabState(e, i)} />
+								// <CloseIcon />
+								// </div>
+							);
+						})}
+					</Tabs>
+				</Paper>
+				{/* This List needs to be a child functional component that gets passed an array  */}
 				<List>
 					{loaded.map(function(i) {
 						return <ListItem key={i.id}>{i.text}</ListItem>;
