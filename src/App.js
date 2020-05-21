84789b39fff4a5f7619c46f90bf713ccd177a1db
daltlc <daltonlcarr@yahoo.com>
2020-05-20 20:20:42 -0700

    stocktwitzapi

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import './App.scss';
 import TwitterBot from './components/TwitterBot';
+import StockTwitz from './components/StockTwitz';
 import Container from '@material-ui/core/Container';
-import { RecoilRoot, atom, selector, useRecoilState, useRecoilValue } from 'recoil';
 
 function App() {
 	return (
 		<div className="App">
-			<RecoilRoot>
-				<Container maxWidth="lg">
-					<TwitterBot />
-				</Container>
-			</RecoilRoot>
+			<Container maxWidth="lg">
+				{/* <TwitterBot /> */}
+				<StockTwitz />
+			</Container>
 		</div>
 	);
 }
---
+++ src/components/StockTwitz.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class StockTwitz extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { loading: false, msg: null };
+	}
+
+	handleClick = (api) => (e) => {
+		e.preventDefault();
+
+		this.setState({ loading: true });
+		axios
+			.get('/.netlify/functions/' + api)
+			.then((response) => response.json())
+			.then((json) => this.setState({ loading: false, msg: json.msg }));
+	};
+
+	render() {
+		const { loading, msg } = this.state;
+
+		return (
+			<p>
+				<button onClick={this.handleClick('fetch-tweets')}>{loading ? 'Loading...' : 'Call Lambda'}</button>
+
+				<span>{msg}</span>
+			</p>
+		);
+	}
+}
